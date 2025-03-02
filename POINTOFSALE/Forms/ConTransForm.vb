211c268072dd04f24c2fed652f65260204a7aa07
Oyi <Tab>
2025-03-03 01:20:46 +0800

    terminal

---
+++ POINTOFSALE/Forms/ConTransForm.vb
@@ -0,0 +1,30 @@
+﻿Public Class ConTransForm
+    Private Sub BtnNo_Click(sender As Object, e As EventArgs) Handles BtnNo.Click
+        Dim res As Integer = MessageBox.Show("To day transaction is continue?", "Question", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+        If res = vbOK Then
+            POSForm.Enabled = True
+            ReadingForm.Enabled = True
+            TransactionForm.dgRecordTrans.Rows.Clear()
+            ReadingForm.Hide()
+            Me.Hide()
+        End If
+    End Sub
+
+    Private Sub BtnYes_Click(sender As Object, e As EventArgs) Handles BtnYes.Click
+        Dim res As DialogResult = MessageBox.Show("Today's transaction is closed", "Information", MessageBoxButtons.OKCancel, MessageBoxIcon.Information)
+
+        If res = DialogResult.OK Then
+            ' Set StopScanActive to True (pause scanning)
+            StopScan.StopScanActive = True
+
+            Dim repo As New OrderRepo
+            repo.ClearRedings()
+            repo.ClearDataGridView()
+
+            ReadingForm.Enabled = True
+            POSForm.Enabled = True
+            ReadingForm.Hide()
+            Me.Hide()
+        End If
+    End Sub
+End Class
--- POINTOFSALE/Forms/ReadingForm.vb
+++ POINTOFSALE/Forms/ReadingForm.vb
@@ -1,21 +1,33 @@
 ﻿Public Class ReadingForm
 
     Dim repo As New OrderRepo
+
     Private Sub ReadingForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         repo.Zread()
     End Sub
-
-
 
     Private Sub c1_TextChanged(sender As Object, e As EventArgs) Handles c1.TextChanged, c5.TextChanged, c10.TextChanged, c20.TextChanged, p50.TextChanged, p100.TextChanged, p200.TextChanged, p500.TextChanged, p1000.TextChanged
         repo.Zread()
     End Sub
 
     Private Sub BtnPost_Click(sender As Object, e As EventArgs) Handles BtnPost.Click
+        If TransactionForm.dgRecordTrans.Rows.Count = 0 OrElse (TransactionForm.dgRecordTrans.Rows.Count = 1 AndAlso TransactionForm.dgRecordTrans.Rows(0).IsNewRow) Then
+            MessageBox.Show("No transactions.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
         repo.SaveTransactionsToDatabase()
+        repo.PrintZread()
+        ConTransForm.Show()
+        Me.Enabled = False
     End Sub
 
     Private Sub BtnPrintDenomination_Click(sender As Object, e As EventArgs) Handles BtnPrintDenomination.Click
+        If TransactionForm.dgRecordTrans.Rows.Count = 0 OrElse (TransactionForm.dgRecordTrans.Rows.Count = 1 AndAlso TransactionForm.dgRecordTrans.Rows(0).IsNewRow) Then
+            MessageBox.Show("No transactions to print.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Return
+        End If
+
         repo.PrintZread()
     End Sub
 
